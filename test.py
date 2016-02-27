68244b15c774e2eb24bb2c1ba269de5d347c271d
Humachine <swarun@gmail.com>
2016-02-27 03:41:24 -0800

    First commit. Simple Python Flask server

---
+++ test.py
@@ -0,0 +1,30 @@
+from flask import Flask, url_for, Response
+import flask
+app = Flask(__name__)
+
+dat = {
+            "id": 1,
+                "name": "A green door",
+                    "price": 12.50,
+                        "tags": ["home", "green"]
+                        }
+
+@app.route('/', methods=['POST'])
+def api_root():
+    tt = flask.jsonify(**dat)
+    print tt
+    print type(tt)
+    return tt
+
+@app.route('/articles')
+def api_articles():
+    return 'List of ' + url_for('api_articles')
+
+@app.route('/articles/<articleid>')
+def api_article(articleid):
+    return 'You are reading ' + articleid
+
+
+if __name__ == '__main__':
+    app.run()
+
