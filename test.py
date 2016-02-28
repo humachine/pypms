cfc3a8777c4cd2b0e3db273dba90da9a228844bb
Humachine <swarun@gmail.com>
2016-02-27 18:26:36 -0800

    Making it send back requests from Heroku app

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from flask import Flask, url_for, Response
 import flask
 import json
+import requests
 app = Flask(__name__)
 
 dat = {
@@ -15,13 +16,27 @@ dat = {
                         "tags": ["home", "green"]
                         }
 
+target = 'https://api.dev.promptapp.io/api/1.0/webhook/@test101_00026'
+headers = {'content-type': 'application/json'}
+headers['Prompt-API-key']='26e805e78f0d8ca28098c14077500a42'
+payload = {'some': 'data'}
+payload['uuid']="517c843897fd4411b70ab3256e39f9da60b14483f0b2893e268fe12d57ac09d1"
+
+
 @app.route('/', methods=['POST'])
 def api_root():
     d = flask.request.data
+    print d
     datadict = json.loads(d)
-    res=datadict['prerequisites']['shippingaddress']['state']
-    dat['returnans']=res*4
+    if datadict['prerequisites']['shippingaddress']['state']:
+        res=datadict['prerequisites']['shippingaddress']['state']
+        dat['returnans']=res*4
     tt = flask.jsonify(**dat)
+
+
+    r=requests.post(target, json.dumps(payload), headers=headers)
+    print r.content
+
     return tt
 
 @app.route('/articles')
